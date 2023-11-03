59d430110a5e4703311d26adb4fdd932b61bc0d8
JonatanRek <gamerclassn7@gmail.com>
2023-11-03 15:19:50 +0100

    livewire Fixes

--- resources/views/components/summernote.blade.php
+++ resources/views/components/summernote.blade.php
@@ -1,48 +1,49 @@
 <div class="mb-3">
-    <textarea class="summernote" name="{{ $name }}" id="{{ $id }}"
-        @if (isset($livewireModel) && !empty($livewireModel)) wire:model={{$livewireModel}} @endif
-    >{!! $value !!}</textarea>
+    <textarea class="summernote" id="{{ $id }}" name="{{ $name }}">{!! $value !!}</textarea>
 
-    @push('scripts')
-        <script>
-            $("#{{ $id }}").summernote({
-                height: {{ $height }},
-                toolbar: [
-                    ['codeview', ['codeview']],
-                    ['style', ['style']],
-                    ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol']],
-                    ['insert', ['picture']],
-                    ['table', ['table']],
-                    ['help', ['help']],
-                ],
-                styleTags: ['p', 'h1', 'h2', 'h3', 'h4', 'h5'],
-                callbacks: {
-                    onPaste: function(e) {
-                        var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData('Text');
-                        e.preventDefault();
-                        setTimeout(function() {
-                            document.execCommand('insertText', false, bufferText);
-                        }, 10);
-                    }
-                },
-                hint: {
-                    mentions: {!! json_encode($mentions) !!},
-                    match: /\B@(\w*)$/,
-                    search: function(keyword, callback) {
-                        callback($.grep(this.mentions, function(item) {
-                            return item.name.toLowerCase().indexOf(keyword.toLowerCase()) == 0;
-                        }));
+    <script>
+        $("#{{ $id }}").summernote({
+            height: {{ $height }},
+            toolbar: [
+                ['codeview', ['codeview']],
+                ['style', ['style']],
+                ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol']],
+                ['insert', ['picture']],
+                ['table', ['table']],
+                ['help', ['help']],
+            ],
+            styleTags: ['p', 'h1', 'h2', 'h3', 'h4', 'h5'],
+            callbacks: {
+                @if (isset($livewireModel) && !empty($livewireModel))
+                    onChange: function($contents, $editable) {
+                        @this.{{ $livewireModel }} = $contents;
                     },
-                    template: function (item) {
-                        return item.name;
-                    },
-                    content: function(item) {
-                        return $('<span>').html('<span data-type="mention" data-mention="'+item.id+'" contenteditable="false">@' + item.name + '</span><span>&nbsp;</span>')[0]
-                    }
+                @endif
+                onPaste: function(e) {
+                    var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData('Text');
+                    e.preventDefault();
+                    setTimeout(function() {
+                        document.execCommand('insertText', false, bufferText);
+                    }, 10);
                 }
-            });
-        </script>
-    @endpush
+            },
+            hint: {
+                mentions: {!! json_encode($mentions) !!},
+                match: /\B@(\w*)$/,
+                search: function(keyword, callback) {
+                    callback($.grep(this.mentions, function(item) {
+                        return item.name.toLowerCase().indexOf(keyword.toLowerCase()) == 0;
+                    }));
+                },
+                template: function(item) {
+                    return item.name;
+                },
+                content: function(item) {
+                    return $('<span>').html('<span data-type="mention" data-mention="' + item.id + '" contenteditable="false">@' + item.name + '</span><span>&nbsp;</span>')[0]
+                }
+            }
+        });
+    </script>
 </div>
