aa559ef62163af6895a4bc9cdd3450c9f6d32123
Patrik Sztefek <patrik.sztefek@gmail.com>
2026-03-07 15:39:17 +0100

    input-price

---
+++ resources/views/components/input-price.blade.php
@@ -0,0 +1,99 @@
+@php
+    $wireModel = $attributes
+        ->whereStartsWith('wire:model')
+        ->filter(fn ($value, $key) => $key !== 'wire:model.vat')
+        ->first() ?? false;
+    $wireVatModel = $attributes->get('wire:model.vat') ?? false;
+    $nameKey = $wireModel ?? $name ?? '';
+@endphp
+
+<div class="{{ $groupClass }}" x-data="{
+    @if($wireModel)
+        priceWithout: $wire.entangle(@js($wireModel)),
+    @else
+        priceWithout: @js(old($name, $value)),
+    @endif
+    inputValue: 0,
+    decimals: {{ $decimals }},
+    @if($wireVatModel)
+        vatPercent: $wire.entangle(@js($wireVatModel)),
+    @else
+        vatPercent: @js($vat),
+    @endif
+    updateInput() {
+        const value = parseFloat(
+            this.mode === 'with' 
+            ? (this.priceWithout * (1 + this.vatRate)) 
+            : this.priceWithout
+        );
+        this.inputValue = Number.isFinite(value) ? value.toFixed(this.decimals) : 0;
+    },
+    updateWithout() {
+        const value = parseFloat(
+            this.mode === 'with' 
+            ? this.inputValue / (1 + this.vatRate) 
+            : this.inputValue
+        );
+        this.priceWithout = Number.isFinite(value) ? value.toFixed(this.decimals) : 0;
+    },
+    get withoutVat() {
+        const value = parseFloat(this.priceWithout);
+        return Number.isFinite(value) ? value.toFixed(this.decimals) : 0;
+    },
+    get withVat() {
+        const value = parseFloat(this.priceWithout) * (1 + this.vatRate);
+        return Number.isFinite(value) ? value.toFixed(this.decimals) : 0;
+    },
+    get vatRate() {
+        return (this.vatPercent ?? 0) / 100;
+    },
+    mode: 'with',
+}" x-init="updateInput(); $watch('vatPercent', () => updateInput())">
+    @if (!empty($label))
+        <label class="form-label"
+            @isset($id) for="{{ $id }}" id="{{ $id }}-label" @endisset
+        >
+            {{ $label }}
+        </label>
+    @endif
+
+    @if(!$wireModel)
+        <input 
+            type="text"
+            name="{{ $name }}"
+            x-model="priceWithout"
+        >
+    @endif
+
+    <div class="input-group">
+        <input
+            type="number"
+            step="{{ $step }}"
+            class="form-control border-end-0 pe-0 {{ $errors->has($nameKey) ? 'is-invalid' : '' }}"
+            x-model="inputValue" x-on:input="updateWithout()"
+        >
+        <div class="input-group-text bg-body ps-0">{{ $currency }}</div>
+        <select class="form-select flex-grow-0 w-26" x-model="mode" x-on:change="updateInput()">
+            <option value="with">{{ __('s DPH') }}</option>
+            <option value="without">{{ __('bez DPH') }}</option>
+        </select>
+    </div>
+
+    <div class="form-text">
+        <template x-if="mode=='without'">
+            <span><span x-text="withVat"></span> {{ $currency }} {{ __('s DPH') }}</span>
+        </template>
+        <template x-if="mode=='with'">
+            <span><span x-text="withoutVat"></span> {{ $currency }} {{ __('bez DPH') }}</span>
+        </template>
+        &shy;
+    </div>
+
+    @error($nameKey)
+        <div class="invalid-feedback" role="alert">{{ $message }}</div>
+    @enderror
+
+    @if (isset($help) && !empty($help))
+        <div class="form-text">{{ $help }}</div>
+    @endif
+</div>
---
+++ src/View/Components/InputPrice.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SteelAnts\Form\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
+use Illuminate\View\Component;
+
+class InputPrice extends Component
+{
+    public string $step = '';
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public $name = null,
+        public $label = null,
+        public mixed $value = null,
+        public $groupClass = null,
+        public $help = null,
+        public $id = null,
+        public int $vat = 21,
+        public string $currency = 'Kč',
+        public int $decimals = 6,
+    ) {
+        $this->id ??= Str::uuid();
+
+        if ($decimals) {
+            $this->step = '.' . str_repeat('0', $decimals - 1) . '1';
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return 'form::components.input-price';
+    }
+}
